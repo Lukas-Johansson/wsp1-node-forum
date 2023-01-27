49c0405ff482b27170f9f2ff6d52881a4ebd0d92
Lukas-Johansson <lukas.johansson4@elev.ga.ntig.se>
2023-01-27 11:16:50 +0100

    Har fixat de problem som har uppståt och fixat skriva saker med css så nu gråter man inte då man skriver saker

---
+++ public/javascript/menu.js
@@ -0,0 +1,8 @@
+const button = document.querySelector(".navbar__button");
+
+button.addEventListener("click", function (event) {
+    console.log(event);
+    const menu = document.querySelector(".navbar__collapse");
+    console.log(menu);
+    menu.classList.toggle("navbar__collapse--show");
+});
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/', async function (req, res) {
-    res.render('index.njk');
-});
-
 const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
@@ -14,10 +9,14 @@ const pool = mysql.createPool({
 });
 const promisePool = pool.promise();
 
-router.post('/new', async function (req, res, next) {
-    const { author, title, content } = req.body;
-    const [rows] = await promisePool.query("INSERT INTO lj04forum (authorId, title, content) VALUES (?, ?, ?)", [author, title, content]);
-    res.redirect('/');
+module.exports = router;
+
+router.get('/', async function (req, res, next) {
+    const [rows] = await promisePool.query("SELECT * FROM lj04forum");
+    res.render('index.njk', {
+        rows: rows,
+        title: 'Forum',
+    });
 });
 
 router.get('/new', async function (req, res, next) {
@@ -28,23 +27,16 @@ router.get('/new', async function (req, res, next) {
     });
 });
 
-router.get('/', async function (req, res, next) {
-    const [rows] = await promisePool.query("SELECT * FROM lj04forum");
-    res.render('index.njk', {
-        rows: rows,
-        title: 'Forum',
-    });;    
-});
-
 router.post('/new', async function (req, res, next) {
     const { author, title, content } = req.body;
-    let user = await promisePool.query('SELECT * FROM lj04user WHERE name = ?', [author]);
+
+    let user = await promisePool.query('SELECT * FROM lj04users WHERE name = ?', [author]);
     if (!user) {
-        user = await promisePool.query('INSERT INTO lj04user (name) VALUES (?)', [author]);
+        user = await promisePool.query('INSERT INTO lj04users (name) VALUES (?)', [author]);
     }
-    const userId = user.insertId || user[0].id;
-    const [rows] = await promisePool.query('INSERT INTO lj04user (author, title, content) VALUES (?, ?, ?)', [userId, title, content]);
+    const userId = user.insertId || user[0][0].id;
+    const [rows] = await promisePool.query('INSERT INTO lj04forum (authorId, title, content) VALUES (?, ?, ?)', [userId, title, content]);
     res.redirect('/'); 
 });
 
-module.exports = router;
+
